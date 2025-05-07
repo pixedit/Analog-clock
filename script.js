a1265b143bd68becae7387184f089d5fe481761d
christmas <pixedit.2023@gmail.com>
2025-05-07 07:48:06 +0330

    make project responsive

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ const secondHand = document.querySelector(".second");
 
 //////////////////////// POSITIONING THE CHARACTERS OF CLOCK ON A SIRCLE
 for (let i = 1; i <= 12; i++) {
-	let angle = (i + 9) * 30; // +9 makes the characters stay at the right position
+	let angle = (i + 9) * 30;
+	// +9 makes the characters stay at the right position
 	// i * 30 because 360 degree of a circle / 12 = 30 degree
 
-	const element = document.createElement("span");
+	const element = document.createElement("div");
 	element.textContent = i;
 	element.classList.add("hour-character");
-
-	element.style.transform = `rotate(${angle}deg) translate(325px) rotate(-${angle}deg)`;
-
 	clock.appendChild(element);
+
+	element.style.setProperty("--angle", `${angle}deg`);
 }
 //////////////////////// UPDATING THE CLOCK HANDS EVERY SECOND
 function updateClock() {
@@ -24,9 +24,8 @@ function updateClock() {
 	const seconds = now.getSeconds();
 
 	const hourDeg = hours * 30 + minutes * 0.5;
-	// 360deg / 12 hours = 30deg and we need to calc minute effect on hour degree too
+	// 360deg / 12 hours = 30deg. we need to calc minute effect on hour degree
 	// 30 / 60 minutes = 0.5deg || hourDeg = hours * 30 + minutes * 0.5
-
 	const minuteDeg = minutes * 6; // 360deg / 60 minutes = 6deg
 	const secondDeg = seconds * 6;
 
